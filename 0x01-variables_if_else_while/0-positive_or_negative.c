3092c6f0fbb7e700ccaffa5296a819c51499005f
IslamAbdelsalam <eabdelsalam@student.eelu.edu.eg>
2023-02-17 15:38:25 -0800

    RTFM +- Betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-int main() {
-    int n;
-    scanf("%i",&n);
-    if (n == 0){
-        printf("%i is zero\n",n);
-    }else if ( n > 0 ){
-        printf("%i is positive\n",n);
-    }else if ( n < 0 ){
-        printf("%i is negative\n",n);
-    }else{
-        return -1;
-    }
+int main(void)
+{
+	int n;
 
-    return 0;
+	scanf("%i", &n);
+
+	if (n == 0)
+		printf("%i is zero\n", n);
+	else if (n > 0)
+		printf("%i  is  positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		return (-1);
+
+	return (0);
 }
