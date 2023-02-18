9ea68b0d3e2ee825255e2b647f5fa55bc2889020
IslamAbdelsalam <eabdelsalam@student.eelu.edu.eg>
2023-02-17 18:10:22 -0800

    Hello, numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar(10);
+
+	return (0);
+}
