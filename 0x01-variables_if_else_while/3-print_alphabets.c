fe4696be9c42e7cc8a0e0a6b6dd436e1d83375a6
IslamAbdelsalam <eabdelsalam@student.eelu.edu.eg>
2023-02-17 18:01:14 -0800

    Double Alphapet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar(10);
+
+	return (0);
+}
