846c3f14c361fc6332e4d0c3a494b97bfb69f858
IslamAbdelsalam <eabdelsalam@student.eelu.edu.eg>
2023-02-17 18:06:30 -0800

    task number 4 that asks me to remove e and q from the result

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+	putchar(10);
+
+	return (0);
+}
