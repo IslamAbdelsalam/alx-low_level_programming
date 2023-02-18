98cd48ec3580cde17c7f7a220732b0781f12bfcb
IslamAbdelsalam <eabdelsalam@student.eelu.edu.eg>
2023-02-17 18:13:06 -0800

    print_tebahpla :) yah it's alphabet but in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar(10);
+
+	return (0);
+}
