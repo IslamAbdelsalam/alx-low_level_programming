41ba49ddfa6d361a9f396c48102e692180a52b76
Islam <islamabdoulradi@gmail.com>
2023-02-21 23:49:25 +0200

    new design for this task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
+
 /**
- *  main -Entry point
- *  Return: Always 0 (Success)
-*/
+ * main - Print _putchar
+ * Return: Always 0
+ */
 int main(void)
 {
-char text[] = "_putchar";
-int i;
-
-for (i = 0 ; text[i] != '\0'; i++)
-{
-putchar(text[i]);
-}
-
-putchar('\n');
-
-return (0);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
