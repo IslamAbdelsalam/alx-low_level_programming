39b235938b44e84d0791e5c8a6c6972f6ca7adac
Islam <islamabdoulradi@gmail.com>
2023-02-22 00:16:27 +0200

    let's print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - will print the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+int i;
+
+for (i = 1; i <= 10; i++)
+print_alphabet();
+
+}
