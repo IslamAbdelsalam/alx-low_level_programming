41ab923e8ad17f681353eea037a26680f53949cd
Islam <islamabdoulradi@gmail.com>
2023-02-22 01:35:38 +0200

    last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - will convert the input to abslute value
+ * @n: the input for my function
+ * Return: n%10 if it's greater than 0,
+ *        -(n%10) if it's smaller than 0
+ *
+*/
+int print_last_digit(int n)
+{
+if (n >= 0)
+return (n % 10);
+else
+return (-(n % 10));
+}
