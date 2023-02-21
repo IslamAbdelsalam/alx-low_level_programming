bf0e40d491a2a509ab3a9010d59ccfd2e16e43fa
Islam <islamabdoulradi@gmail.com>
2023-02-22 01:17:01 +0200

    greater-equal-smaller than 0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - will check is it greater than 0 or smaller than 0 or equal to 0
+ * @n: the input for my function
+ * Return: 1 if it's greater than 0,
+ *         0 if it's equal to 0
+ *        -1 if it smaller than 0
+ *
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+
+
+}
