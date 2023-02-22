136497d6afe86e15d6f9d41378f20c2082131f07
Islam <islamabdoulradi@gmail.com>
2023-02-22 04:31:01 +0200

    solve_102_again

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,19 +2,18 @@
 
 /**
  * fibonacci - print first 50 fab number
- * @n: our input from user
 */
 
-void fibonacci(int n)
+void fibonacci(void)
 {
-double n1 = 1, n2 = 2, product = 0, i;
+unsigned long long int n1 = 1, n2 = 2, product = 0, i;
 printf("1, 2\n");
 for (i = 0; i < 50; i++)
 {
 product = n1 + n2;
 n1 = n2;
 n2 = product;
-printf("%0.0lf", product);
+printf("%lli", product);
 if (i < 50)
 {
 printf(", ");
