fc651825e7cf7b0f18cc2fff0001e37f65ada773
Islam <islamabdoulradi@gmail.com>
2023-02-22 03:32:37 +0200

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @num1: first number
+ * @num2: second number
+ * Return: num1 + num2 for summition
+*/
+
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
