3ab7d152f29e53e49e82b769bf4f45141f183cfc
Islam <islamabdoulradi@gmail.com>
2023-02-22 03:23:18 +0200

    last try

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,39 +3,29 @@
 /**
  * times_table - let's print time table
 */
+
 void times_table(void)
 {
-int i, j, mult;
+	int num, mult, prod;
 
-for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-mult = i * j;
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
 
-if (mult > 9)
-{
-putchar(mult / 10 + '0');
-putchar(mult % 10 + '0');
-}
-else
-{
-_putchar(mult + '0');
-}
-if (j != 9)
-{
-_putchar(',');
-if (mult < 10)
-{
-putchar(' ');
-putchar(' ');
-}
-else
-_putchar(' ');
-}
-/* comment*/
-}
-_putchar('\n');
-}
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
 
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
---
+++ 0x02-functions_nested_loops/90-mytry_to_solve_num_9.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - let's print time table
+*/
+void times_table(void)
+{
+int i, j, mult;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+mult = i * j;
+
+if (mult > 9)
+{
+putchar(mult / 10 + '0');
+putchar(mult % 10 + '0');
+}
+else
+{
+_putchar(mult + '0');
+}
+if (j != 9)
+{
+_putchar(',');
+if (mult < 10)
+{
+putchar(' ');
+putchar(' ');
+}
+else
+_putchar(' ');
+}
+/* comment*/
+}
+_putchar('\n');
+}
+
+}
