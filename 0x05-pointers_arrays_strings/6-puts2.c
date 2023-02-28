84d14c030bfd23556847365b996f3167243b3d27
Islam <islamabdoulradi@gmail.com>
2023-02-28 22:31:45 +0200

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * puts2 - print our even char in our string
+ * @str: pointer to our string
+ */
+void puts2(char *str)
+{
+	int size = 0, i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+		size++;
+	size--;
+
+	for( j = 0; j < size; j++ )
+	{
+		_putchar(str[j]);
+	}
+}
