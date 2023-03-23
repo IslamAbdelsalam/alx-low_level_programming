346cb463235d279e39e1bb9a384b539fb8259eb3
Islam <islamabdoulradi@gmail.com>
2023-03-23 16:22:09 +0200

    33bd

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,11 +15,11 @@ char *_strncpy(char *dest, char *src, int n)
 	int dest_len = strlen(dest);
 	int i;
 
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for (; i < n; i++)
 		dest[i] = '\0';
 
-	return (dest);
 
+	return (dest);
 }
