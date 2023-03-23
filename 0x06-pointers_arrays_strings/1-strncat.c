045e81b67572383bd63407dbace2d204ad29b0d8
Islam <islamabdoulradi@gmail.com>
2023-03-23 15:33:30 +0200

    Solve _strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - this function will concat 2 string but in length n
+ *
+ * @src: pointer to the source of string
+ *
+ * @dest: pointer to the destenation that we will copy string to
+ *
+ * @n: lenth of string that we will copy to dest
+ *
+ * Return: our destenation that we will concat the string in
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+size_t dest_len = strlen(dest);
+	size_t i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+
+}
