d86d3b5c099a150bc4c7b54937ee037233884525
Islam <islamabdoulradi@gmail.com>
2023-03-23 16:17:30 +0200

    solve strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,15 @@
  *
  * Return: our destenation that we will concat the string in
  */
-char *_strncpy(char *dest, char *src, int n){
+char *_strncpy(char *dest, char *src, int n)
+{
 	int dest_len = strlen(dest);
 	int i;
 
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 
