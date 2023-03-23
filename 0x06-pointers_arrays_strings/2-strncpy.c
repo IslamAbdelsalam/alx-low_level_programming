321345dcef930f2f959fbbc06c92a6ab6562b720
Islam <islamabdoulradi@gmail.com>
2023-03-23 15:41:45 +0200

    Solve _strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - this function will concat 2 string but in length n
+ *
+ * @src: pointer to the source of string
+ *
+ * @dest: pointer to the destenation that we will copy string to
+ *
+ * @n: lenth of string that we will copy to dest
+ *
+ * Return: our destenation that we will concat the string in
+ */
+char *_strncpy(char *dest, char *src, int n){
+	int dest_len = strlen(dest);
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[i] = src[i];
+
+	return (dest);
+
+}
