b627a64ad0dade8082edcc9d4a45aafebc1dee02
Islam <islamabdoulradi@gmail.com>
2023-06-10 01:50:17 +0300

    ff

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,10 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include<string.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -26,5 +24,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif /* solo */
+#endif /* MAIN_H */
