8e418125199ebeefd3d75390e818e92e8cda989d
Islam <islamabdoulradi@gmail.com>
2023-06-10 03:14:50 +0300

    argv to void

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - our main function
+ *
+ * @argc: argument counter
+ *
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%i\n", (argc - 1));
+	return (0);
+}
