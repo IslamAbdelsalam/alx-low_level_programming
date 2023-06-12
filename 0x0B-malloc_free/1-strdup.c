7e322f31a5ce79de2350fafbc588fcf29353c55b
Islam <islamabdoulradi@gmail.com>
2023-06-12 19:57:34 +0300

    solve 2th problem

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *_strdup - this function will create arry of characters
+ *				and copy string from input into this new array'string'
+ *
+ * @str: our string we need to copy it to new array.
+ *
+ * Return: pointer to our array
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int size = strlen(str), i;
+
+	char *str_arr = malloc(size * sizeof(char));
+
+	if (size == 0 || str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; str[i] != '\0'; ++i)
+			str_arr[i] = str[i];
+
+		str_arr[i + 1] = '\0';
+	}
+
+	return (str_arr);
+
+}
