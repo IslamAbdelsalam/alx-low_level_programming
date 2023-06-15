fa0f139981f4b78fd3432c8d185d7c083d28f510
Islam <islamabdoulradi@gmail.com>
2023-06-16 02:15:03 +0300

    main test

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,6 @@
  * task 0
  */
 #include "main.h"
-#include <stdlib.h>
 /**
  * malloc_checked - allocates memory using malloc.
  * @b: array size
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,7 +3,5 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 void *malloc_checked(unsigned int b);
-
 #endif
