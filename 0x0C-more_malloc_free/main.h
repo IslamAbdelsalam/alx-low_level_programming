8631234ab1bb080daed9d5180545de19abf6a8f8
Islam <islamabdoulradi@gmail.com>
2023-06-16 02:17:16 +0300

    dd

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,11 @@
-#ifndef FILE_MAIN
+#ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <stdlib.h>
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
-#endif
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+#endif /* Solo */
