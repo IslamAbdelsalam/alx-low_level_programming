49c07280ace6734908dcd140fe624bc1e681712b
Islam <islamabdoulradi@gmail.com>
2023-06-20 05:37:42 +0300

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*Contents of the header file*/
+/***
+ * struct 'dog' - Represents info about dog
+ *
+ * @name: name of the dog
+ * @age: Dog age
+ * @owner: Dog owner name
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /*HEADER_FILE_NAME_H */
