e17f157d450480005060ce2abb304d2e8d531ad4
Islam <islamabdoulradi@gmail.com>
2023-06-22 04:12:38 +0300

    3359

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - function pointer point to other function to perform
+ * the operation asked by the user
+ * @s: user input
+ *
+ * Return: pointer to the correct function that perform the exact order
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
