eef1abf0f9bfe65ce8b46a73ebf49d3c6b873752
Islam <islamabdoulradi@gmail.com>
2023-06-22 04:01:56 +0300

    333

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,14 +13,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
+	if (!(array && cmp))
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
+		if (cmp(*(array + i)))
 		{
-			if (cmp(array[i]) != 0)
-				return (i);
+			return (i);
 		}
 	}
-
 	return (-1);
 }
