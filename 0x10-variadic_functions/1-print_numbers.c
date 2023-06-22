7d90b5129581291bebfd29dd6ca0aaa124221953
Islam <islamabdoulradi@gmail.com>
2023-06-23 02:39:24 +0300

    solve task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,43 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - will print the arguments with the
+ *					separator is found...
+ * @n: Number of arguments
+ * @separator: Arguments separator
+ *
+ * Return: non.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	if (separator == NULL)
+	{
+		int x;
+
+		for (i = 0; i < n - 1; i++)
+		{
+			x = va_arg(args, int);
+			printf("%d ", x);
+		}
+		x = va_arg(args, int);
+		printf("%d", x);
+	}
+	else
+	{
+		int x;
+
+		for (i = 0; i < n - 1; i++)
+		{
+			x = va_arg(args, int);
+			printf("%d%s", x, separator);
+		}
+		x = va_arg(args, int);
+		printf("%d", x);
+	}
+	printf("\n");
+}
