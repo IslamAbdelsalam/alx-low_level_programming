e5b85873f0e9ae4e3c31cb1b0c7c3c0ad4870563
Islam <islamabdoulradi@gmail.com>
2023-07-01 17:39:49 +0300

    add to the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+ * add_node_end - add node to the end of linked list
+ * @str: node str content
+ * @head: pointer to pointer to the head of the linked list
+ * Return: the new node
+ * Author: Islam Abdelslam
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+	
+	if (str == NULL)
+		return (NULL);
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		list_t *current = *head;
+
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+
+		current->next = new_node;
+	}
+
+	return (new_node);
+}
