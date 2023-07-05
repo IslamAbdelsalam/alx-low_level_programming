7de19d08cfb16399c806667a300ea89f0039ed13
Islam <islamabdoulradi@gmail.com>
2023-07-05 03:16:12 +0300

    Freeeee

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - free the linked list
+ * @head: pointer to the head of the linked list
+ * Author: Islam Abdelslam
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	while (head->next != NULL)
+	{
+		free(head->next);
+		head->next = NULL;
+	}
+	free(head);
+}
