50029b9322d9c260d90fdfccf3f835b12ac24859
Islam <islamabdoulradi@gmail.com>
2023-07-06 03:33:31 +0300

    hh

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,8 +19,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	nthNode->next = NULL;
 	listLen = getListLen(*head);
 
-	if (*head == NULL)
-		return (nthNode);
+	/*if (*head == NULL)
+		return (nthNode);*/
 	if (listLen + 1 < idx)
 		return (NULL);
 	else if (listLen == idx)
