8fac6a083f521bb4edd7d3dae24ffcfe5389a0a7
Islam <islamabdoulradi@gmail.com>
2023-07-06 03:54:01 +0300

    last push now

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		current->next = nthNode;
 	}
-	else if (listLen > idx)
+	else if (listLen + 1 > idx)
 	{
 		current = *head;
 		if (idx == 0)
