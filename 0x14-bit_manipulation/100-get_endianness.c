d46e9c9ab3d9738dd3cd6d8a55861ee49fe8d0a8
Islam <islamabdoulradi@gmail.com>
2023-07-07 05:17:54 +0300

    test code

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks if a machine is little or big endian
+ * Return: 0 for big endian, 1 for little endian
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	char *c = (char *)&n;
+
+	return (*c);
+}
