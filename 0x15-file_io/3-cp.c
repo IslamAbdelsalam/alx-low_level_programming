8896097fa8b9786c5767c6f5f27e171798f51f5e
Islam <islamabdoulradi@gmail.com>
2023-07-11 12:01:35 +0300

    Hello cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include "main.h"
+/**
+ * copy - it will operate like cp in terminal
+ * @source: path to source file
+ * @target: path to past the source copy
+ *
+ */
+void copy(char *source, char *target)
+{
+	int o_source, o_target, checkWrite, checkClose;
+	char *buffer[1024];
+	ssize_t n;
+	mode_t new_perms = 0664;
+
+	o_source = open(source, O_RDONLY);
+	if (o_source == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
+
+	o_target = open(target, O_CREAT | O_RDWR | O_TRUNC);
+	if (o_target == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+		exit(99);
+	}
+	while ((n = read(o_source, buffer, sizeof(buffer))) > 0)
+	{
+		checkWrite = write(o_target, buffer, n);
+		if (checkWrite == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+			exit(99);
+		}
+	}
+	if (o_source == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
+	checkClose = close(o_source);
+	if (checkClose == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
+		exit(100);
+	}
+	chmod(target, new_perms);
+	close(o_target);
+}
+
+/**
+ * main - main function --start--
+ * @argc: argument counter
+ * @argv: pointer to argument vector
+ *
+ * Return: (0) if it success
+ * Author: Islam Abdelslam
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	copy(argv[1], argv[2]);
+
+	return (0);
+}
