29642511eb86476dbf6b42e5deea70d45b79904e
Islam <islamabdoulradi@gmail.com>
2023-07-11 04:17:19 +0300

    creat

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: relative path to the file or the name of it.
+ * @text_content: text that er will write to our file
+ *
+ * Return: 1 on success
+ *			-1 on: file can not be created,
+ *				file can not be written, write “fails”
+ *				filename is NULL
+ *
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int f, w;
+	char *buffer;
+	size_t cnt;
+
+	cnt = strlen(text_content);
+	if (filename == NULL)/* checked */
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		creat(filename, 0600);
+		return (1);
+	}
+
+	buffer = (char *)malloc(sizeof(char) * (strlen(text_content) + 10));
+
+	if (buffer == NULL)/* checked */
+		return (-1);
+
+	f = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	w = write(f, buffer, cnt);
+
+	if (f == -1 || w == -1)
+	{
+		close(f);
+		free(buffer);
+		return (-1);
+	}
+
+	close(f);
+	free(buffer);
+	return (1);
+}
